60678314c893e19b99c9944a490509682248e8f3
yauhescha <evgescha.maslov007@yandex.ru>
2021-01-19 10:56:49 +0300

    Добавлены сервлеты для работы с проеподователем

---
+++ src/net/codejava/javaee/bookstore/TeacherMDeleteServlet.java
@@ -0,0 +1,53 @@
+package net.codejava.javaee.bookstore;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.codejava.javaee.bookstore.DAO.GroupMDAO;
+import net.project.entity.GroupM;
+
+public class TeacherMDeleteServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private GroupMDAO DAO;
+
+	public void init() {
+		String jdbcURL = getServletContext().getInitParameter("jdbcURL");
+		String jdbcUsername = getServletContext().getInitParameter("jdbcUsername");
+		String jdbcPassword = getServletContext().getInitParameter("jdbcPassword");
+
+		DAO = new GroupMDAO(jdbcURL, jdbcUsername, jdbcPassword);
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		doGet(request, response);
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		try {
+			delete(request, response);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	private void delete(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException {
+		int id = Integer.parseInt(request.getParameter("id"));
+
+		GroupM entity = new GroupM(id);
+		DAO.delete(entity);
+		response.sendRedirect("teacher");
+
+	}
+
+}
---
+++ src/net/codejava/javaee/bookstore/TeacherMUpdateServlet.java
@@ -0,0 +1,63 @@
+package net.codejava.javaee.bookstore;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.codejava.javaee.bookstore.DAO.TeacherMDAO;
+import net.project.entity.TeacherM;
+
+public class TeacherMUpdateServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private TeacherMDAO DAO;
+
+	public void init() {
+		String jdbcURL = getServletContext().getInitParameter("jdbcURL");
+		String jdbcUsername = getServletContext().getInitParameter("jdbcUsername");
+		String jdbcPassword = getServletContext().getInitParameter("jdbcPassword");
+
+		DAO = new TeacherMDAO(jdbcURL, jdbcUsername, jdbcPassword);
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		doGet(request, response);
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		try {
+			if (request.getParameter("id") != null)
+				update(request, response);
+			else
+				insert(request, response);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	private void insert(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException {
+		String name = request.getParameter("name");
+
+		TeacherM entity = new TeacherM(name);
+		DAO.insert(entity);
+		response.sendRedirect("teacher");
+	}
+
+	private void update(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException {
+		int id = Integer.parseInt(request.getParameter("id"));
+		String name = request.getParameter("name");
+		TeacherM entity = new TeacherM(id, name);
+		DAO.update(entity);
+		response.sendRedirect("teacher");
+	}
+
+}
